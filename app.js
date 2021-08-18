977e8544c0c1d05248a1ad9140235f766c584de7
mdshohidulislam1486 <74629151+mdshohidulislam1486@users.noreply.github.com>
2021-08-18 19:25:17 +0600

    git commmited

--- app.js
+++ app.js
@@ -14,3 +14,36 @@ function generatePin(){
     const pin = getPin();
     pinOutPut = document.getElementById('generated-pin').value = pin
 }
+
+document.getElementById('key-pad').addEventListener('click', function(event){
+const number = event.target.innerText;
+const calcInput = document.getElementById('typed-num');
+if(isNaN(number)){
+    if(number == 'C'){
+        calcInput.value = '';
+    }
+} else{
+    const calcInput = document.getElementById('typed-num');
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number
+    calcInput.value = newNumber;
+}
+
+})
+
+
+function verifyPin(){
+    const pin = document.getElementById('generated-pin').value;
+    const typedNumbers = document.getElementById('typed-num').value;
+    const successNotification  = document.getElementById('pin-success');
+    const failedNotification  = document.getElementById('pin-failed');
+
+    if (pin == typedNumbers){
+        
+        successNotification.style.display = 'block'
+        failedNotification.style.display = 'none'
+    } else {
+        successNotification.style.display = 'none'
+        failedNotification.style.display = 'block'
+    }
+}
